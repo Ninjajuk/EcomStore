ad657c4bb5b9a6c037856c7032b4eb890d7d1b19
SAMSUDDIN ANSARI <108830220+Ninjajuk@users.noreply.github.com>
2024-01-05 09:08:30 +0530

    Update PaymentMethod.js

--- src/Components/Checkout/PaymentMethod.js
+++ src/Components/Checkout/PaymentMethod.js
@@ -40,7 +40,7 @@ function PaymentMethod() {
               htmlFor="card"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Card Payment
+             credit Card Payment 
             </label>
           </div>
         </div>
