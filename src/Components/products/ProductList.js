74e9a00ca9cf03aea8494a7d121fc7c4a659ccbd
SAMSUDDIN ANSARI <108830220+Ninjajuk@users.noreply.github.com>
2024-01-07 00:16:48 +0530

    Update ProductList.js

--- src/Components/products/ProductList.js
+++ src/Components/products/ProductList.js
@@ -1,19 +1,6 @@
 import AdminLayout from "../AdminDashboard/AdminLayout";
 
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
+
 const products = [
   {
     id: 1,
